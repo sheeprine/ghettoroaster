4f92ef8cc61de89e495cc149b0aee9cffe9bf92b
Stéphane Albert <sheeprine@oh.its.fake.nullplace.com>
2019-02-26 07:12:32 +0100

    Add base screen handling class

---
+++ src/screen.cpp
@@ -0,0 +1,50 @@
+#include "screen.h"
+
+
+void Screen::setRoastStatus(roastStates status) {
+    roastStatus = status;
+}
+
+void Screen::useCelsius(void) {
+    isCelsius = true;
+}
+
+void Screen::useFarhenheit(void) {
+    isCelsius = false;
+}
+
+void Screen::setWIFIStatus(bool enabled) {
+    wifiEnabled = enabled;
+}
+
+void Screen::setET(double temp) {
+    ET = temp;
+}
+
+void Screen::setBT(double temp) {
+    BT = temp;
+}
+
+void Screen::setST(double temp) {
+    ST = temp;
+}
+
+void Screen::setFAN(unsigned int dutyCycle) {
+    fanDutyCycle = dutyCycle;
+}
+
+void Screen::setROR(double ror) {
+    ROR = ror;
+}
+
+void Screen::setRINT(unsigned int rorInterval) {
+    RINT = rorInterval;
+}
+
+void Screen::setDuration(unsigned long duration) {
+    roastDuration = duration;
+}
+
+void Screen::refresh(void) {
+    doRefresh();
+}
---
+++ src/screen.h
@@ -0,0 +1,41 @@
+#ifndef SCREEN_H
+#define SCREEN_H
+
+enum roastStates {
+    STOPPED = 0,
+    DRY,
+    FC,
+    FCE,
+    SC,
+    DROP,
+    MAILL,
+    FIN,
+};
+
+class Screen {
+    private:
+        virtual void doRefresh(void) = 0;
+    protected:
+        bool wifiEnabled = false;
+        double ET, BT, ST, ROR = 0;
+        unsigned int fanDutyCycle, RINT = 0;
+        unsigned long roastDuration = 0;
+        roastStates roastStatus = STOPPED;
+        bool isCelsius = true;
+    public:
+        virtual ~Screen();
+        void setRoastStatus(roastStates status);
+        void useCelsius(void);
+        void useFarhenheit(void);
+        void setWIFIStatus(bool enabled);
+        void setET(double temp);
+        void setBT(double temp);
+        void setST(double temp);
+        void setFAN(unsigned int dutyCycle);
+        void setROR(double ror);
+        void setRINT(unsigned int rorInterval);
+        void setDuration(unsigned long duration);
+        void refresh(void);
+};
+
+#endif
