6e0d418187f2e6931b072996573a6452ca04fba5
Stéphane Albert <sheeprine@oh.its.fake.nullplace.com>
2019-04-09 18:10:41 +0200

    Update ESP8266/nodemcu pin mapping

--- src/platforms/esp8266/config.h
+++ src/platforms/esp8266/config.h
@@ -17,27 +17,27 @@ limitations under the License.
 #ifndef ESP8266_CONFIG_H
 #define ESP8266_CONFIG_H
 
-// nodemcu v0.9 pin mapping
-// A0 -> Usable
-// D0 -> Usable
+// nodemcu pin mapping
+//  A0 -> Usable
+//  D0 -> Usable (/!\ HIGH at boot)
 // ~D1 -> SDA
 // ~D2 -> SCL
-// ~D3 -> Usable
-// ~D4 -> Usable
+// ~D3 -> Usable (/!\ Used at boot)
+// ~D4 -> Usable (/!\ Send serial debug data at boot, can't be LOW at boot)
 // ~D5 -> SPI CLK
 // ~D6 -> SPI Q
 // ~D7 -> SPI D (Not connected)
-// ~D8 -> SPI CS (Not connected)
+// ~D8 -> SPI CS -> Usable (/!\ Should not be HIGH at boot)
 // ~D9 -> Serial RX
 // ~D10 -> Serial TX
 
 // Thermocouples CS Pins
-#define TC_ET_PIN A0
-#define TC_BT_PIN D0
+#define TC_ET_PIN D3
+#define TC_BT_PIN D4
 
 // Fan and heat control
-#define FAN_PWM_PIN D3
-#define HEATER_PIN  D4
+#define FAN_PWM_PIN D0
+#define HEATER_PIN  D8
 
 // Modbus addresses
 #define ROAST_ENABLE_ADDR   100
